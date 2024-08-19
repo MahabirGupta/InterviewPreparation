eac9b18ce09d0cd83a530329169515d4ef6bfe79
MahabirGupta <mahabirg81@gmail.com>
2024-08-19 22:12:33 +0800

    Implement the Pre-Order Traversal in the Recursion Algorithm

---
+++ src/RecursionAlgorithms/PreOrderTraversal.java
@@ -0,0 +1,19 @@
+package RecursionAlgorithms;
+
+import javax.swing.tree.TreeNode;
+
+public class PreOrderTraversal {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public void preOrder(TreeNode node) {
+//        if (node != null) {
+//            System.out.print(node.value + " ");
+//            preOrder(node.left);
+//            preOrder(node.right);
+//        }
+//    }
+
+}
