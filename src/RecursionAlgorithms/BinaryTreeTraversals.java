407bf03377389ba3963f21565d73c30e1eed6a97
MahabirGupta <mahabirg81@gmail.com>
2024-08-19 22:08:44 +0800

    Implement the Binary Tree Traversals in the Recursion Algorithm

---
+++ src/RecursionAlgorithms/BinaryTreeTraversals.java
@@ -0,0 +1,19 @@
+package RecursionAlgorithms;
+
+import javax.swing.tree.TreeNode;
+
+public class BinaryTreeTraversals {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public static void inOrder(TreeNode node) {
+//        if (node != null) {
+//            inOrder(node.left);
+//            System.out.print(node.value + " ");
+//            inOrder(node.right);
+//        }
+//    }
+
+}
