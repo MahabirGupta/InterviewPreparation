137b662de45ea2a0d0e47e7c1e24ed59f6245822
MahabirGupta <mahabirg81@gmail.com>
2024-08-19 11:18:29 +0800

    Implement the Selection Sort Sorting Algorithm

---
+++ src/SortingAlgorithms/SelectionSort.java
@@ -0,0 +1,28 @@
+package SortingAlgorithms;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {10,2,36,4,57};
+        selectionSort(arr);
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
