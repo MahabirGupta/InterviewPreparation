6700e69c259c61b6c628c62aca5bd099338166bc
MahabirGupta <mahabirg81@gmail.com>
2024-08-19 22:30:47 +0800

    Implement the Post-Order Traversal in the Recursion Algorithm

---
+++ src/RecursionAlgorithms/PostOrderTraversal.java
@@ -0,0 +1,19 @@
+package RecursionAlgorithms;
+
+import javax.swing.tree.TreeNode;
+
+public class PostOrderTraversal {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public static void postOrder(TreeNode node) {
+//        if (node != null) {
+//            postOrder(node.left);
+//            postOrder(node.right);
+//            System.out.print(node.value + " ");
+//        }
+//    }
+
+}
