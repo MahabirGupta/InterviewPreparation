b48eece12bc5b2d8232be277d7e43b3dbf4cf466
MahabirGupta <mahabirg81@gmail.com>
2024-08-25 18:19:44 +0800

    Implement FibonacciSequence using iterativeApproach

--- src/RecursionAlgorithms/FibonacciSequence.java
+++ src/RecursionAlgorithms/FibonacciSequence.java
@@ -10,6 +10,9 @@ public class FibonacciSequence {
         int memoization = fibonacci(5,memo);
         System.out.println(memoization);
 
+        int iterativeApproach = fibonacciIterativeApproach(7);
+        System.out.println(iterativeApproach);
+
 
     }
 
@@ -23,10 +26,13 @@ public class FibonacciSequence {
 //F(3) = F(2) + F(1) = 1 + 1 = 2
 //F(4) = F(3) + F(2) = 2 + 1 = 3
 //F(5) = F(4) + F(3) = 3 + 2 = 5
+//F(6) = F(5) + F(4) = 5 + 3 = 8
+//F(7) = F(6) + F(5) = 5 + 8 = 13
 //        0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
         return (fibonacci(n - 1) + fibonacci(n - 2));
     }
 
+//    Memoization (Top-Down Dynamic Programming)
     public static int fibonacci(int n, int[] memo) {
         if (n <= 1) {
             return n;
@@ -36,6 +42,21 @@ public class FibonacciSequence {
         }
         return memo[n];
     }
+
+//    3. Iterative Approach (Bottom-Up Dynamic Programming)
+    public static int fibonacciIterativeApproach(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] fib = new int[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
+    }
+
 
 
 }
