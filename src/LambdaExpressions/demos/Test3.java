81c9b52037aa1d6cfc9297a1261ba05e6add65cd
MahabirGupta <mahabirg81@gmail.com>
2024-08-29 17:35:52 +0800

    Implement lambda expression using functional interface with 2 parameters and 1 return type

---
+++ src/LambdaExpressions/demos/Test3.java
@@ -0,0 +1,27 @@
+package LambdaExpressions.demos;
+
+@FunctionalInterface
+interface Cab3{
+    public String bookCab(String source,String destination);
+}
+
+class Ryde implements Cab3{
+
+    @Override
+    public String bookCab(String source, String destination) {
+        System.out.println("Gojek cab is booked from " + source + " to " + destination);
+        return "Price: $6.50";
+    }
+}
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        Cab3 ryde = (source, destination) -> {
+            System.out.println("Gojek cab is booked from " + source + " to " + destination);
+            return "Price: $6.50";
+        };
+        System.out.println(ryde.bookCab("Toa Payoh","Clementi"));
+//        ryde.bookCab("Toa Payoh","Clementi");
+    }
+}
