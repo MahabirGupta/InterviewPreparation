3796c2a517f5719146ea9b24906c5b4101ef1cf2
MahabirGupta <mahabirg81@gmail.com>
2024-08-29 18:06:49 +0800

    Implement lambda expression using Predicate functional interface with 1 parameter and 1 return type to check if given string is greater than 4

--- src/LambdaExpressions/demos/Main.java
+++ src/LambdaExpressions/demos/Main.java
@@ -9,15 +9,16 @@ import java.util.function.Predicate; //pre-defined functionalinterface
 public class Main {
 
     public static void main(String[] args) {
-//        Ex1: check if the number is greater than 4
+//        Ex1: check if the number is greater than 10
         Predicate<Integer> predicate = (i) -> (i>10);
         System.out.println(predicate.test(12));
 //        predicate.test(10);
         System.out.println(predicate.test(2));
 ////        Ex2: check if the length of the given string is greater than 4
-//        Predicate<String> predicate1 = (string) -> (string.length() > 4);
+        Predicate<String> predicate1 = (string) -> (string.length() > 4);
+        System.out.println(predicate1.test("Anish"));
 //        predicate1.test("Anish");
-
+        System.out.println(predicate1.test("Boy"));
 
     }
 }
