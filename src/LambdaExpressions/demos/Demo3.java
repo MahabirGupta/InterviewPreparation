0ad97d6fa3dda2ea901d2f6eb98b6678155e537a
MahabirGupta <mahabirg81@gmail.com>
2024-08-29 20:37:03 +0800

    Implement lambda expression using Predicate functional interface with 1 parameter of type ArrayList and 1 return type to check if employee object's salary is greater than 500 and employee object's experience is greater than 3 print name of employee using for loop and for each loop

---
+++ src/LambdaExpressions/demos/Demo3.java
@@ -0,0 +1,50 @@
+package LambdaExpressions.demos;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+class Employee1 {
+    String ename;
+    int salary;
+    int experience;
+
+    Employee1(String ename, int salary, int experience) {
+        this.ename = ename;
+        this.salary = salary;
+        this.experience = experience;
+    }
+
+    @Override
+    public String toString() {
+        return ename + " " + salary + " " + experience;
+    }
+}
+
+public class Demo3 {
+    public static void main(String[] args) {
+        ArrayList<Employee1> employees = new ArrayList<>();
+        employees.add(new Employee1("John", 10000, 8));
+        employees.add(new Employee1("David", 8500, 4));
+        employees.add(new Employee1("Sam", 4300, 1));
+        employees.add(new Employee1("Vera", 2700, 7));
+
+        // Define the Predicate once
+        Predicate<Employee1> predicate = employee -> (employee.salary > 5000) && (employee.experience > 3);
+
+        // Using for loop
+        for (int i = 0; i < employees.size(); i++) {
+            if (predicate.test(employees.get(i))) {
+                System.out.println(employees.get(i));
+            }
+        }
+
+        System.out.println();
+
+        // Using enhanced for loop
+        for (Employee1 eachEmployee : employees) {
+            if (predicate.test(eachEmployee)) {
+                System.out.println(eachEmployee.ename + " " + eachEmployee.salary);
+            }
+        }
+    }
+}
